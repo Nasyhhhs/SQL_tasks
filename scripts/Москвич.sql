50ffb04a45351275e0ac382488612f27ab3b7cdf
Nastya <ananseets@gmail.com>
2023-08-10 14:32:22 +0300

    rename script

--- scripts/Москвич.sql
+++
@@ -1,8 +0,0 @@
-select l."Возраст гражданина",l."Пол гражданина",l."Телефон", r."Москвич"
-from age_gender l
-left join 
-(select "Телефон", case when "Регистрация" = 'Москва' then 1 else 0 
-end as "Москвич" 
-from registration ) r 
-on l."Телефон" = r."Телефон"
-where r."Телефон" is not null and r."Москвич" = 1
